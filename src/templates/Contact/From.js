e0a7974a8ec07eeb90720ec827db2f83546e59be
emamyari <emamyari@yahoo.com>
2023-08-26 18:15:28 +0330

    develop contact form

--- src/templates/Contact/From.js
+++ src/templates/Contact/From.js
@@ -1,64 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormInput from "../../components/UI/Input";
 
 const From = () => {
+    const [stateName,SetStateName]=useState("")
+    const [stateFamily,SetStateFamily]=useState("")
+    const [stateTel,SetStateTel]=useState("")
+    const [stateEmail,SetStateEmail]=useState("")
+    const [stateText,SetStateText]=useState("")
+
+    function sendToServer(){
+        console.log(stateName)
+        console.log(stateFamily)
+        console.log(stateTel)
+        console.log(stateEmail)
+        console.log(stateText)
+    }
+
+
     return (
         <div className="contact-form-wrap">
-            <form id="contact-form">
-                <div className="row">
+                 <div className="row">
                     <div className="col-md-6">
-                        <FormInput
-                            tag={'input'}
-                            type={'text'}
-                            name={'first_name'}
-                            placeholder={'نام *'}
-                        />
+                        <div className="single-input-item">
+                            <label>
+                                <input onChange={e=>SetStateName(e.target.value)} placeholder='محمود    ' />
+                            </label>
+                        </div>
                     </div>
 
                     <div className="col-md-6">
-                        <FormInput
-                            tag={'input'}
-                            type={'text'}
-                            name={'last_name'}
-                            placeholder={'نام خانوادگی *'}
-                        />
+                        <div className="single-input-item">
+                            <label>
+                                <input  onChange={e=>SetStateFamily(e.target.value)} placeholder='احمدی نژاد ' />
+                            </label>
+                        </div>
+
                     </div>
 
                     <div className="col-md-6">
-                        <FormInput
-                            tag={'input'}
-                            type={'email'}
-                            name={'email_address'}
-                            placeholder={'ایمیل *'}
-                        />
+                        <div className="single-input-item">
+                            <label>
+                                <input onChange={e=>SetStateTel(e.target.value)} placeholder='09123420142' />
+                            </label>
+                        </div>
                     </div>
 
                     <div className="col-md-6">
-                        <FormInput
-                            tag={'input'}
-                            type={'text'}
-                            name={'phone_no'}
-                            placeholder={'تلفن'}
-                        />
+                        <div className="single-input-item">
+                            <label>
+                                <input onChange={e=>SetStateEmail(e.target.value)} placeholder='emamyari@yahoo.com' />
+                            </label>
+                        </div>
                     </div>
 
                     <div className="col-12">
-                        <FormInput
-                            tag={'textarea'}
-                            name={'con_message'}
-                            placeholder={'متن پیام *'}
-                        />
+                        <div className="single-input-item">
+                            <label>
+                                <textarea onChange={e=>SetStateText(e.target.value)} cols="30" rows="7" placeholder='متن نامه ' required />
+                            </label>
+                        </div>
 
-                        <FormInput
-                            tag={'button'}
-                            classes={'btn-outline'}
-                        />
+                        <div className="single-input-item">
+                            <label>
+                                <button onClick={sendToServer} className={`btn-outline  `}>ارسال پیام</button>
 
-                        <div className="form-message"/>
+                            </label>
+                        </div>
+
+                        <div className="form-message" />
                     </div>
                 </div>
-            </form>
-        </div>
+         </div>
     );
 };
 
